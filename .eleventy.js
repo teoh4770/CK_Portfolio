ce76bcf176409cc55ac05026658827404f484a57
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-01 00:02:28 -0400

    ADD | Moving the styles and some content here from the original portfolio site

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
   eleventyConfig.addPlugin(eleventySass);
 
   return {
